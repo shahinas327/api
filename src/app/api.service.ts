de94b1885962006b305922e0d3a132c3c1560e48
shahinas327 <112380003+shahinas327@users.noreply.github.com>
2022-11-29 22:58:12 +0530

    api

---
+++ src/app/api.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient)
+   { }
+   fetchpassenger=()=>{return this.http.get("https://api.instantwebtools.net/v1/passenger?page=0&size=30")}
+}
